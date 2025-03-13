388c8467a21206f541a38ea0780d46e669709544
Rastele <unrastele@gmail.com>
2025-03-13 15:40:29 +0100

    Added one Cypress test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,28 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+
+describe('Swiper Gallery Descriptions Titles Test', function () {
+  it('Check if descriptions match the correct titles', function () {
+    const slides = [
+      { title: 'Rome', description: 'Italy' },
+      { title: 'London', description: 'United Kingdom' },
+      { title: 'Paris', description: 'France' }
+    ];
+
+    cy.visit('http://localhost:3000');
+
+
+    slides.forEach((slide, index) => {
+      cy.get('.swiper-slide-active').should('contain', slide.title);
+      cy.get('.swiper-slide-active').should('contain', slide.description);
+
+      if (index < slides.length - 1) {
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000);
+      }
+      
+    });
+  });
+});
